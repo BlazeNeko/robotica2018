dd6ef9ee2e3f3942f4ca690740669b421ff53809
jocarroza <sergiobs93@gmail.com>
2018-10-16 17:17:30 +0200

    component 3

---
+++ practica3/src/CMakeFiles/move.dir/cmake_clean.cmake
@@ -0,0 +1,38 @@
+file(REMOVE_RECURSE
+  "move_autogen"
+  "CMakeFiles/move_autogen.dir/AutogenOldSettings.cmake"
+  "CommonBehavior.cpp"
+  "CommonBehavior.h"
+  "DifferentialRobot.cpp"
+  "DifferentialRobot.h"
+  "GenericBase.cpp"
+  "GenericBase.h"
+  "Laser.cpp"
+  "Laser.h"
+  "RCISMousePicker.cpp"
+  "RCISMousePicker.h"
+  "ui_mainUI.h"
+  "CMakeFiles/move.dir/specificworker.cpp.o"
+  "CMakeFiles/move.dir/specificmonitor.cpp.o"
+  "CMakeFiles/move.dir/home/salabeta/robocomp/classes/rapplication/rapplication.cpp.o"
+  "CMakeFiles/move.dir/home/salabeta/robocomp/classes/sigwatch/sigwatch.cpp.o"
+  "CMakeFiles/move.dir/home/salabeta/robocomp/classes/qlog/qlog.cpp.o"
+  "CMakeFiles/move.dir/main.cpp.o"
+  "CMakeFiles/move.dir/genericmonitor.cpp.o"
+  "CMakeFiles/move.dir/commonbehaviorI.cpp.o"
+  "CMakeFiles/move.dir/genericworker.cpp.o"
+  "CMakeFiles/move.dir/rcismousepickerI.cpp.o"
+  "CMakeFiles/move.dir/CommonBehavior.cpp.o"
+  "CMakeFiles/move.dir/DifferentialRobot.cpp.o"
+  "CMakeFiles/move.dir/GenericBase.cpp.o"
+  "CMakeFiles/move.dir/Laser.cpp.o"
+  "CMakeFiles/move.dir/RCISMousePicker.cpp.o"
+  "CMakeFiles/move.dir/move_autogen/mocs_compilation.cpp.o"
+  "../bin/move.pdb"
+  "../bin/move"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/move.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ practica3/src/CMakeFiles/move_autogen.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "move_autogen"
+  "CMakeFiles/move_autogen.dir/AutogenOldSettings.cmake"
+  "CMakeFiles/move_autogen"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/move_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
